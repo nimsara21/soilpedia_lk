0058e3c0c38fe5d02b05d3cc2bfd2f2ba908569a
Dinun <dinun.20200155@iit.ac.lk>
2022-04-12 15:39:05 +0530

    Added

--- lib/screen/signin_screen.dart
+++ lib/screen/signin_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:soilpedia_lk/pages/homePage.dart';
+import 'package:soilpedia_lk/pages/navPages/camTest.dart';
+import 'package:soilpedia_lk/pages/navPages/gallery.dart';
 import 'package:soilpedia_lk/pages/navPages/mainPage.dart';
 import 'package:soilpedia_lk/screen/home_screen.dart';
 import 'package:soilpedia_lk/screen/reset_password.dart';
